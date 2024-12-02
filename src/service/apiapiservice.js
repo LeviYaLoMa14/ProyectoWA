7855160893e5f4c9498fac84afb7f22f395c0599
Levi Lopez <rihay1401@gmail.com>
2024-12-02 12:26:18 -0600

    version gpt

--- src/service/apiapiservice.js
+++ src/service/apiapiservice.js
@@ -1,36 +1,47 @@
 const https = require("https");
-function EnviarMensajeWhatsApp(texto, numero){
+
+function EnviarMensajeWhatsApp(texto, numero) {
     const data = JSON.stringify({
-        
-            "messaging_product": "whatsapp",    
-            "recipient_type": "individual",
-            "to": "522371083638",
-            "type": "text",
-            "text": {
-                "preview_url": false,
-                "body": "Hola Ly"
-            }
+        "messaging_product": "whatsapp",
+        "recipient_type": "individual",
+        "to": "522371083638", // Usar número en formato internacional
+        "type": "text",
+        "text": {
+            "preview_url": false,
+            "body": texto // Mensaje dinámico
+        }
     });
-    
+
     const options = {
-        host : "graph.facebook.com",
-        path : "/v21.0/428428697029903/messages",
-        method : "POST",
-        body : data,
-        headers : {
-            "Content-Type" : "aplication/json",
-            Authorization : "Bearer EAASDD0WFtPoBO6fg7w3NB091WGh6dmgGGVE0ZAkxkZC9kyPgZCXcTz61XZBZAl5asfdl85cGVmsndP0q0JaqoZBknkAn3ZBHZAuaTUOwC1NAaiOwepqgg7ws9JeGXx5d1EqFJcziOf0Q53TRqS6FGjqszafyCRw28Cgg537TKcUG1MmtKKZBkZAivdHizEr5bTxXnDRwDu4S5S0hwHou54uxBew8h1VPQZD"
+        host: "graph.facebook.com",
+        path: "/v21.0/428428697029903/messages", // Reemplaza con tu ID correcto
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json", // Corrección en el tipo de contenido
+            "Authorization": "Bearer EAASDD0WFtPoBO6fg7w3NB091WGh6dmgGGVE0ZAkxkZC9kyPgZCXcTz61XZBZAl5asfdl85cGVmsndP0q0JaqoZBknkAn3ZBHZAuaTUOwC1NAaiOwepqgg7ws9JeGXx5d1EqFJcziOf0Q53TRqS6FGjqszafyCRw28Cgg537TKcUG1MmtKKZBkZAivdHizEr5bTxXnDRwDu4S5S0hwHou54uxBew8h1VPQZD"
         }
     };
-    const req = https.request(options,res => {
-        res.on("data",d=>{
-            process.stdout.write(d);
+
+    const req = https.request(options, (res) => {
+        let response = "";
+
+        res.on("data", (chunk) => {
+            response += chunk;
+        });
+
+        res.on("end", () => {
+            console.log("Respuesta de la API:", response);
         });
     });
+
+    req.on("error", (error) => {
+        console.error("Error al enviar el mensaje:", error);
+    });
+
     req.write(data);
     req.end();
 }
 
 module.exports = {
     EnviarMensajeWhatsApp
-}
+};
